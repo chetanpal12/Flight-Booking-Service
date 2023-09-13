90d120fcb14494eb4c9ecf5b7ed27f72e3091c79
chetan pal <cpal22387@gmail.com>
2023-09-13 13:51:07 +0530

    started booking implementation

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,5 +4,6 @@ const path = require('path');
 dotenv.config({path:path.join(__dirname,'../../.env')}); 
 
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    FLIGHT_SERVICE:process.env.FLIGHT_SERVICE
 }
--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,22 @@
+const{BookingService}=require('../services');
+const {StatusCodes}=require('http-status-codes');
+const {ErrorResponce,SuccessResponce}=require('../utils/common')
+async function createBooking(req,res){
+    try {
+        console.log(req.body)
+        const responce=await BookingService.createBooking({
+            flightId:req.body.flightId,
+            userId:req.body.userId,
+            noofSeats:req.body.noofSeats
+        });
+        SuccessResponce.data=responce;
+        return res.status(StatusCodes.OK).json({SuccessResponce});
+    } catch (error) {
+        ErrorResponce.error=error;
+        console.log(error)
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ErrorResponce});
+    }
+}
 module.exports={
-    
+    createBooking
 }
